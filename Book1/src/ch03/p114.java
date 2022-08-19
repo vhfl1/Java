7d1b08b7be08acfadd6c567f114c51c0da0aed7d
dpadptmxl3 <dpadptmxl3@gmail.com>
2022-08-19 13:49:57 +0900

    2

---
+++ Book1/src/ch03/p114.java
@@ -0,0 +1,13 @@
+package ch03;
+
+public class p114 {
+
+	public static void main(String[] args) {
+		int i;
+		int sum;
+		for(i =1, sum = 0; i <= 10; i++){
+			sum += i;
+		}
+		System.out.println("1부터 10까지의 합은"+sum+"입니다.");
+	}
+}
---
+++ Book1/src/ch03/p116.java
@@ -0,0 +1,14 @@
+package ch03;
+
+public class p116 {
+	public static void main(String[] args) {
+		
+	}
+	
+	
+	
+	
+	
+	
+}
+		
