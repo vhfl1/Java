5e93f884ae917ba77beb8d57567ce0a9d361454d
dpadptmxl3 <dpadptmxl3@gmail.com>
2022-08-22 11:00:29 +0900

    a

--- Book1/src/ch03/p116.java
+++ Book1/src/ch03/p116.java
@@ -2,7 +2,14 @@ package ch03;
 
 public class p116 {
 	public static void main(String[] args) {
+		int dan;
+		int times;
 		
+		for(times = 1; times <10 ; times++) {
+			
+			System.out.println(dan+ "X" + times + "=" + dan * times);
+		}
+		System.out.println();
 	}
 	
 	
