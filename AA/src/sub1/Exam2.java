8cd2d64d5fbb6341eda26bcdddb4698483879c04
dpadptmxl3 <dpadptmxl3@gmail.com>
2022-08-30 12:46:38 +0900

    수행평가1

---
+++ AA/src/sub1/Exam2.java
@@ -0,0 +1,24 @@
+package sub1;
+
+import java.util.Scanner;
+public class Exam2 {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		for(int i = 1; i <= num; i++) {
+		
+			for(int j = num-1; j >= i; j--) {
+			System.out.print(" ");
+		}
+			for(int j = 1; j <= i; j++) {
+	    	System.out.print("*");
+	    }
+		System.out.println("");
+	}
+
+ }	
+}
