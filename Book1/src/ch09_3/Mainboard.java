5f8c5313c70d5ab2dd23d468a5af765dd3e369c8
dpadptmxl3 <dpadptmxl3@gmail.com>
2022-09-01 13:42:34 +0900

    ch09

---
+++ Book1/src/ch09_3/Mainboard.java
@@ -0,0 +1,19 @@
+package ch09_3;
+
+public class Mainboard {
+
+	public static void main(String[] args) {
+		
+		Player player = new Player();
+		player.play(1);
+		
+		AdvancedLevel aLevel = new AdvancedLevel();
+		player.upgradeLevel(aLevel);
+		player.play(2);
+		
+		SuperLevel sLevel = new SuperLevel();
+		player.upgradeLevel(sLevel);
+		player.play(3);
+		
+	}
+}
---
+++ Book1/src/ch09_3/Player.java
@@ -0,0 +1,24 @@
+package ch09_3;
+
+public class Player {
+
+	private PlayerLevel level;
+	
+	public Player() {
+		level = new BeginnerLevel();
+		level.showLevelMessage();
+	}
+	
+	public PlayerLevel getlevel() {
+		return level;
+	}
+	
+	public void upgradeLevel(PlayerLevel level) {
+		this.level = level;
+		level.showLevelMessage();
+	}
+	
+	public void play(int count) {
+		level.go(count);
+	}
+}
