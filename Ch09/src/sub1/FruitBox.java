b800088ec74eac5cdd138e58090d4697679c5a1a
dpadptmxl3 <dpadptmxl3@gmail.com>
2022-09-08 11:33:18 +0900

    0908

---
+++ Ch09/src/sub1/FruitBox.java
@@ -0,0 +1,17 @@
+package sub1;
+
+import java.util.Set;
+
+public class FruitBox<T> {
+
+	private T fruit;
+	
+	
+	public T getFruit() {
+		return fruit;
+	}
+	
+	public void setFruit(T fruit) {
+		this.fruit = fruit;
+	}
+}
---
+++ Ch09/src/sub2/StackTest.java
@@ -0,0 +1,26 @@
+package sub2;
+
+import java.util.Stack;
+
+/*
+ * 날짜 : 2022/09/08
+ * 이름 : 윤사랑
+ * 내용 : 자료구조 스택 실습하기
+ */
+public class StackTest {
+
+	public static void main(String[] args) {
+		
+		Stack<Integer> stack = new Stack<>();
+		
+		stack.push(1);
+		stack.push(2);
+		stack.push(3);
+		stack.push(4);
+		stack.push(5);
+		
+		while(!stack.empty()) {
+			System.out.println(stack.pop());
+		}
+	}
+}
