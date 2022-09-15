df2d79ca220e9a109a62508f2245590506b6a5c1
dpadptmxl3 <dpadptmxl3@gmail.com>
2022-09-15 13:46:28 +0900

    0915

---
+++ Book1/src/ch11_2/StringTest2.java
@@ -0,0 +1,17 @@
+package ch11_2;
+
+public class StringTest2 {
+
+	public static void main(String[] args) {
+		
+		String javaStr = new String("java");
+		String androidStr = new String("android");
+		System.out.println(javaStr);
+		System.out.println("처음 문자열 주소값 : "+System.identityHashCode(javaStr));
+		
+		javaStr = javaStr.concat(androidStr);
+		
+		System.out.println(javaStr);
+		System.out.println("연결된 문자열 주소값 : "+System.identityHashCode(javaStr));
+	}
+}
