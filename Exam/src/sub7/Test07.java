8518afd7da8faa1729e0b89e4d3e94ca765eb0bd
dpadptmxl3 <dpadptmxl3@gmail.com>
2022-09-28 10:49:41 +0900

    0928

---
+++ Exam/src/sub7/Test07.java
@@ -0,0 +1,42 @@
+package sub7;
+/*
+ * 날짜 : 2022/09/28
+ * 이름 : 윤사랑
+ * 내용 : 자바 총정리 연습문제
+ */
+
+interface Player{
+
+	public void play();
+	
+}
+		
+class BaseBallPlayer implements Player{
+	@Override
+	public void play() {
+		System.out.println("야구를 합니다.");
+	}
+}
+		
+class FootBallPlayer implements Player{
+	@Override
+	public void play() {
+		System.out.println("축구를 합니다");
+	}
+}
+
+public class Test07 {
+
+	public static void main(String[] args) {
+		
+		BaseBallPlayer p1 = new BaseBallPlayer();
+		FootBallPlayer p2 = new FootBallPlayer();
+		
+		playGame(p1);
+		playGame(p2);
+	}
+	
+	public static void playGame() {
+		p.play();
+	}
+}
---
+++ Exam/src/sub7/Test10.java
@@ -0,0 +1,15 @@
+package sub7;
+/*
+ * 날짜 : 2022/09/28
+ * 이름 : 윤사랑
+ * 내용 : 자바 총정리 연습문제
+ */
+class Human{
+	public void print() {
+		System.out.println("사람입니다.");
+	}
+}
+
+public class Test10 {
+
+}
